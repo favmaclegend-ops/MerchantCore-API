4cb7017a3c9d23f2be4a7a9c0a24435092a042fb
Favour Macauley <favmac007@gmail.com>
2026-05-05 22:06:39 +0000

    Add SSL support for Aiven MySQL

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+connect_args = {}
+if "aiven" in settings.DATABASE_URL.lower():
+    connect_args["ssl"] = {"ca": "/etc/ssl/certs/ca-certificates.crt"}
+
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
